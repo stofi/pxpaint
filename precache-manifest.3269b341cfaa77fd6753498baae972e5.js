625331d414e75d7c1c51b90910fc209e587dc448
Kryštof Rainisch <krystof.rainisch@sprites.co>
2019-07-27 14:27:23 +0200

    deploy

---
+++ precache-manifest.3269b341cfaa77fd6753498baae972e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8640fda52101c7e1b780",
+    "url": "/pxpaint/css/app.8d3fe0d8.css"
+  },
+  {
+    "revision": "8640fda52101c7e1b780",
+    "url": "/pxpaint/js/app.928ff7d3.js"
+  },
+  {
+    "revision": "5b3663e82d1b0a0a1ecb",
+    "url": "/pxpaint/js/chunk-vendors.cd4d03f6.js"
+  },
+  {
+    "revision": "00b31b4e45b4d27927af21354ad16813",
+    "url": "/pxpaint/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pxpaint/robots.txt"
+  }
+];
